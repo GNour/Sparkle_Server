678dee43be61ebb190a7cca23581d090d37fd2fd
Ghyath Noureddine <ghyathnour@gmail.com>
2021-11-12 15:30:50 +0200

    Fix seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,10 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\Course;
-use App\Models\Quiz;
-use App\Models\Team;
-use App\Models\Todo;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 
@@ -20,25 +16,15 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $user = new User();
-        $user->username = "admin";
-        $user->first_name = "admin";
-        $user->last_name = "admin";
-        $user->email = "admin@gmail.com";
+        $user->username = "Sparkle.Admin";
+        $user->first_name = "SparkleTMS";
+        $user->last_name = "Admin";
+        $user->email = "admin@sparkletms.tk";
         $user->password = "$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi";
         $user->gender = 0;
-        $user->role = "Admin";
+        $user->role = "Manager";
         $user->phone_number = "+961 78844775";
         $user->profile_picture = "default.png";
         $user->save();
-
-        Course::factory(4)
-            ->hasTasks(random_int(1, 3))
-            ->hasVideos(random_int(1, 3))
-            ->hasArticles(random_int(1, 3))
-            ->has(Quiz::factory()->hasQuestions(3))
-            ->create();
-        Todo::factory(10)->hasTasks(random_int(0, 3))->create();
-
-        Team::factory(3)->hasMembers(4)->create();
     }
 }
